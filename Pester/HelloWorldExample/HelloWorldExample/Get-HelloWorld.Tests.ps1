b8c0b96b0c8c0ac743ed23a1d7b84b8c8ac63767
Tsuyoshi Ushio <tsushi@microsoft.com>
2016-12-19 23:22:23 +0900

    initial commit

---
+++ Pester/HelloWorldExample/HelloWorldExample/Get-HelloWorld.Tests.ps1
@@ -0,0 +1,9 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Get-HelloWorld" {
+    It "outputs 'Hello World!" {
+        Get-HelloWorld | Should Be 'Hello World!'
+    }
+}
